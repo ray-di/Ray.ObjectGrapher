5806fcfcf8149d7e55306b85ef8832a9ae4a800b
Akihito Koriyama <akihito.koriyama@gmail.com>
2019-09-29 08:09:51 +0900

    add setter injector in provider

--- tests/Fake/PdoProvider.php
+++ tests/Fake/PdoProvider.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Ray\ObjectGrapher;
 
+use Ray\Di\Di\Inject;
 use Ray\Di\Di\Named;
 use Ray\Di\ProviderInterface;
 
@@ -13,6 +14,13 @@ class PdoProvider implements ProviderInterface
      * @Named("dsn=dsn,id=id,pass=pass")
      */
     public function __construct(string $dsn, string $id, string $pass)
+    {
+    }
+
+    /**
+     * @Inject
+     */
+    public function setFoo(FooInterface $foo)
     {
     }
 
