332e76a5b4f0d1c088b917b1ab1bda7ff59ed8e0
Akihito Koriyama <akihito.koriyama@gmail.com>
2019-09-28 09:14:43 +0900

    add setter fake

--- tests/Fake/DatabaseLogger.php
+++ tests/Fake/DatabaseLogger.php
@@ -4,9 +4,18 @@ declare(strict_types=1);
 
 namespace Ray\ObjectGrapher;
 
+use Ray\Di\Di\Inject;
+
 class DatabaseLogger implements LoggerInterface
 {
     public function __construct(PdoInterface $pdo)
+    {
+    }
+
+    /**
+     * @Inject
+     */
+    public function setFoo(FooInterface $foo)
     {
     }
 }
---
+++ tests/Fake/Foo.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Ray\ObjectGrapher;
+
+class Foo implements FooInterface
+{
+}
