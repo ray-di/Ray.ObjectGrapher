7d8a3cab19d42fb2e6505f3e575bcc12a978bb69
Akihito Koriyama <akihito.koriyama@gmail.com>
2020-01-18 07:13:25 +0900

    remove table space

--- src/InstanceNode.php
+++ src/InstanceNode.php
@@ -32,9 +32,9 @@ final class InstanceNode implements NodeInterface
     public function __construct(string $id, string $interface, string $named, Instance $instance)
     {
         $this->id = $id;
-        $this->interface = htmlspecialchars(str_replace('\\', '\\\\', $interface));
+        $this->interface = addslashes($interface);
         $this->named = $named ? /* @lang html */ "@<font color=\"#000000\" point-size=\"10\">{$named}<br align=\"left\"/></font>" : '';
-        $this->type = mb_strimwidth(addslashes((string) $instance), 0, 48, '..');
+        $this->type = getType($instance->value);
     }
 
     public function __toString()
@@ -42,11 +42,11 @@ final class InstanceNode implements NodeInterface
         $html = /* @lang html */ <<< EOT
 {$this->id} [style=dashed, margin=0.02, label=
 <<table cellspacing="0" cellpadding="5" cellborder="0" border="0">
-    <tr>
-        <td align="left" port="header" bgcolor="#aaaaaa">{$this->named}
-            <font point-size="11" color="#333333">{$this->interface}<br align="left"/></font>
-            <font color="#000000">instance {$this->type}<br align="left"/></font>
-        </td>
+<tr>
+<td align="left" port="header" bgcolor="#aaaaaa">{$this->named}
+<font point-size="11" color="#333333">{$this->interface}<br align="left"/></font>
+<font color="#000000">instance ($this->type)<br align="left"/></font>
+</td>
 </tr>
 </table>>, shape=box]
 EOT;
